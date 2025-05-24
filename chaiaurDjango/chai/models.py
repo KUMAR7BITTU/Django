6ed34389c8502f26b5d52b2c69bec77cb1196f92
Bittu Kumar <bittuk@essbbsr.esspl.com>
2025-05-24 16:27:29 +0530

    all ojects of database loaded in html

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -14,6 +14,7 @@ class Chaivariety(models.Model):
    image = models.ImageField(upload_to='chais/')
    date_added = models.DateTimeField(default=timezone.now)
    type = models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
+   description = models.TextField(default='')
 
    def __str__(self): # To see the name of object in admin panel
       return self.name
