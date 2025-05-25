472c8d23b874cb6e0d9d7fcaa51b8768371b1b5e
Bittu Kumar <bittuk@essbbsr.esspl.com>
2025-05-25 11:46:41 +0530

    link added in button in http://127.0.0.1:8000/chai/

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Chaivariety
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def all_chai(request):
     chais = Chaivariety.objects.all()
     return render(request,'chai/all_chai.html',{'chais':chais})
+
+def chai_detail(request,chai_id):
+    chai = get_object_or_404(Chaivariety,pk=chai_id)
+    return render(request,'chai/chai_detail.html',{'chai':chai})
